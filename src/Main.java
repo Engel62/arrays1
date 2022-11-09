8f74c244a0e74e1e56d2a1b949799db1fb20fc6b
Engel62 <sr.engel@bk.ru>
2022-11-09 16:44:41 +0300

    наименования массивов переделано

--- src/Main.java
+++ src/Main.java
@@ -2,42 +2,42 @@ public class Main {
     public static void main(String[] args) {
         /// задание 1,2
         System.out.println("Задание 1,2");
-        int [] a = new int[3];
-        a[0] = 1;
-        a[1] = 2;
-        a[2] = 3;
-        for (int i = 0; i < a.length; i++)
-            System.out.print(a[i]+ ",");
+        int [] integerArray = new int[3];
+        integerArray[0] = 1;
+        integerArray[1] = 2;
+        integerArray[2] = 3;
+        for (int i = 0; i < integerArray.length; i++)
+            System.out.print(integerArray[i]+ ",");
         System.out.print("\n");
 
-        double [] b = {1.57, 7.654, 9.986};
-        for (int d = 0; d < b.length; d++)
-            System.out.print(b[d]+ ",");
+        double [] arrayOfFractionalNumbers = {1.57, 7.654, 9.986};
+        for (int d = 0; d < arrayOfFractionalNumbers.length; d++)
+            System.out.print(arrayOfFractionalNumbers[d]+ ",");
         System.out.print("\n");
 
-        int [] c = {1, 2 ,3 ,4 ,5 ,6 ,7 ,8,};
-        for (int f = 0; f < c.length; f++)
-            System.out.print(c[f] + "," );
+        int [] arbitraryArray = {1, 2 ,3 ,4 ,5 ,6 ,7 ,8,};
+        for (int f = 0; f < arbitraryArray.length; f++)
+            System.out.print(arbitraryArray[f] + "," );
         System.out.print("\n");
         /// Задание 3
         System.out.println("Задание 3");
-        for (int i = a.length - 1; i >= 0; i--)
-            System.out.print(a[i]+ ",");
+        for (int i = integerArray.length - 1; i >= 0; i--)
+            System.out.print(integerArray[i]+ ",");
         System.out.print("\n");
 
-        for (int d = b.length - 1; d >= 0; d--)
-            System.out.print(b[d]+ ",");
+        for (int d = arrayOfFractionalNumbers.length - 1; d >= 0; d--)
+            System.out.print(arrayOfFractionalNumbers[d]+ ",");
         System.out.print("\n");
 
-        for (int f = c.length - 1; f >= 0; f--)
-            System.out.print(c[f] + "," );
+        for (int f = arbitraryArray.length - 1; f >= 0; f--)
+            System.out.print(arbitraryArray[f] + "," );
         System.out.print("\n");
         /// Задание 4
         System.out.println("Задание 4");
-        for (int i = 0; i < a.length; i++){
-            if (a[i] % 2 != 0)
-            a[i] = a[i] + 1;
-            System.out.println(a[i]);
+            for (int i = 0; i < integerArray.length; i++){
+                if (integerArray[i] % 2 != 0)
+            integerArray[i] = integerArray[i] + 1;
+                System.out.print(integerArray[i] + ",");
             }
     }
 }
